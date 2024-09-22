e4eea644583309562630f76868bbffa539aa84b8
harshu6397 <harshkumarkardam258@gmail.com>
2024-09-22 18:30:10 +0530

    complete service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,47 @@
-const Services = () => {
-  return (
-    <div>Services Page</div>
-  )
-}
+"use client";
 
-export default Services
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import services from "../../data/services";
+
+const Services = () => {
+    return (
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: {
+                            delay: 2.4,
+                            duration: 0.4,
+                            ease: "easeIn",
+                        },
+                    }}
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                >
+                    {services.map(({ name, description, num, path }) => (
+                        <div key={num} className="flex-1 flex flex-col gap-6 justify-center group">
+                          {/* top */}
+                            <div className="w-full flex justify-between items-center">
+                                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{num}</div>
+                                <Link href={path} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                                    <BsArrowDownRight className="text-primary text-3xl" />
+                                </Link>
+                            </div>
+                            {/* title */}
+                            <h2 className="h2 font-bold leading-none group-hover:text-accent transition-all duration-500">{name}</h2>
+                            {/* description */}
+                            <p>{description}</p>
+                            {/* border */}
+                            <div className="border-b border-white/20 w-full"></div>
+                        </div>
+                    ))}
+                </motion.div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;
---
+++ data/services.jsx
@@ -0,0 +1,32 @@
+const services = [
+    {
+        num: "01",
+        name: "Web Development",
+        description:
+            "I build websites and web applications using the latest technologies and frameworks.",
+        path: "/services/web-development",
+    },
+    {
+        num: "02",
+        name: "Backend Development",
+        description:
+            "I build backend applications using the latest technologies and frameworks.",
+        path: "/services/backend-development",
+    },
+    {
+        num: "03",
+        name: "Mobile Development",
+        description:
+            "I build mobile applications using the latest technologies and frameworks.",
+        path: "/services/mobile-development",
+    },
+    {
+        num: "04",
+        name: "Data Science",
+        description:
+            "I build data science projects using the latest technologies and frameworks.",
+        path: "/services/data-science",
+    },
+];
+
+export default services;
